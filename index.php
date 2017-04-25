31b69c63e26c72cc4bbf81f6462855cd574d82f7
nifx87 <nifx87@gmail.com>
2017-04-25 18:25:08 +0300

    1.0

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ switch ($message)
 {
     case '/start';
         $message = "Бот является демонстрацией";
-        sendMessage($token, $id, $message);
+        sendMessage($token, $id, $message.prymKeyMeny());
         break;
     case 'off';
         $message = "Вы отказалиь от рассылки";
@@ -29,7 +29,7 @@ switch ($message)
         $message = "333";
         sendMessage($token, $id, $message);
         break;
-    case '/stop';
+    case 'Выход';
         $message = "До скорой встречи!";
         sendMessage($token, $id, $message);
         break;
@@ -47,3 +47,15 @@ function sendMessage($token,$id,$message)
 }
 
 file_put_contents('logs.txt',$output);
+
+/**
+ *
+ */
+function prymKeyMeny()
+{
+    $prbtn =[['Выбрать товар'],['ПРАЙС'],['Выход']];
+    $keyboard = json_encode($keyboard=['keyboard'=>$prbtn,'resize_keyboard'=> true,'one_time_keyboard'=> false,'selective'=> true]);
+    $reply_markup = '&reply_markup='.$keyboard.'';
+
+    return $reply_markup;
+}
