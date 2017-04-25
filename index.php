2e4fa90c345f2cc95013be69a6d6595e0b235e57
nifx87 <nifx87@gmail.com>
2017-04-25 18:40:33 +0300

    1.0

--- index.php
+++ index.php
@@ -23,19 +23,31 @@ switch ($message)
         break;
     case '55';
         $message = "555";
-        sendMessage($token, $id, $message);
+        sendMessage($token, $id, $message.secondKeyMeny());
         break;
     case '33';
         $message = "333";
-        sendMessage($token, $id, $message);
+        sendMessage($token, $id, $message.secondKeyMeny());
         break;
     case 'Выход';
         $message = "До скорой встречи!";
         sendMessage($token, $id, $message);
         break;
+    case 'Выбрать товар';
+        $message = "555";
+        sendMessage($token, $id, $message.secondKeyMeny());
+        break;
+    case 'ПРАЙС';
+        $message = "333";
+        sendMessage($token, $id, $message.secondKeyMeny());
+        break;
+    case 'Главное меню';
+        $message = "Бот является демонстрацией";
+        sendMessage($token, $id, $message.prymKeyMeny());
+        break;
     default:
         $message = "Я отдыхаю";
-        sendMessage($token, $id, $message);
+        sendMessage($token, $id, $message.prymKeyMeny());
         break;
 
 }
@@ -54,8 +66,17 @@ file_put_contents('logs.txt',$output);
 function prymKeyMeny()
 {
     $prbtn =[['Выбрать товар'],['ПРАЙС'],['Выход']];
-    $keyboard = json_encode($keyboard=['keyboard'=>$prbtn,'resize_keyboard'=> true,'one_time_keyboard'=> false,'selective'=> true]);
+    $keyboard = json_encode($keyboard=['keyboard'=>$prbtn,'resize_keyboard'=> true,'one_time_keyboard'=> true,'selective'=> true]);
     $reply_markup = '&reply_markup='.$keyboard.'';
 
     return $reply_markup;
+}
+
+function secondKeyMeny()
+{
+    $secbtn =[['Главное меню'],['33'],['44'],['55'],['66'],['77'],['88'],['Назад']];
+    $keyboard2 = json_encode($keyboard2=['keyboard'=>$secbtn,'resize_keyboard'=> true,'one_time_keyboard'=> true,'selective'=> true]);
+    $reply_markup2 = '&reply_markup='.$keyboard2.'';
+
+    return $reply_markup2;
 }
