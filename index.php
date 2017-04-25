af6f51aaf147b400a5f956d785bb683943c8ea92
nifx87 <nifx87@gmail.com>
2017-04-25 14:56:30 +0300

    1.0

--- index.php
+++ index.php
@@ -5,5 +5,10 @@
  * Date: 25.04.2017
  * Time: 12:49
  */
+$url ="https://api.telegram.org/bot" ;
+$token ="324117481:AAEtL6PPZmnQ8gYCsyJeC2Rf1AUJ9GuG8GA" ;
+$start = "/start";
 $output = json_decode(file_get_contents('php://input'),true);
-file_put_contents('logs.txt',$output);
+$message = $output['maessage'];
+$id = $output['message']['chst_id'];
+file_put_contents('logs.txt',$message,$id);
