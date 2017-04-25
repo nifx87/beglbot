02041bfb09e35a7afb93a076f3b7b435d2a0a2c4
nifx87 <nifx87@gmail.com>
2017-04-25 15:27:57 +0300

    1.0

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
  * Time: 12:49
  */
 $url ="https://api.telegram.org/bot" ;
-$token ="" ;
+$token ="324117481:AAEtL6PPZmnQ8gYCsyJeC2Rf1AUJ9GuG8GA" ;
 $start = "/start";
 $output = json_decode(file_get_contents('php://input'),true);
-$text = $output['maessage']['text'];
-//$id = $output['message']['chst']['id'];//
-file_put_contents('logs.txt',$text);
+$id = $output['message']['chst']['id'];
+
+function sendMessage($url,$token,$id)
+{
+    file_get_contents($url.$token."/sendMessage?chat_id=".$id."&trext=Магазин временно не работает.")
+}
+
+file_put_contents('logs.txt',$output);
