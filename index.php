4ed72cbdb87865d5ea576d0f496fb5f1c444c194
nifx87 <nifx87@gmail.com>
2017-04-25 14:15:51 +0300

    1.0

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
  * Date: 25.04.2017
  * Time: 12:49
  */
-$output = file_get_contents('php://input');
+$output = json_decode(file_get_contents('php://input'),true);
 file_put_contents('logs.txt',$output);
